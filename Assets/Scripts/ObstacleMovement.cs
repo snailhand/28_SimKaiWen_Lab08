c7dbd1ebf7e4aaa36f9efb3be8db2419ed173082
simkaiwen <kaiphonegt@gmail.com>
2022-02-10 17:57:18 +0800

    Updated ObstacleMovement script

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
-    private float xSpeed = -4f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float xSpeed;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(xSpeed*Time.deltaTime, 0, 0f));
+        transform.Translate(new Vector3(-xSpeed*Time.deltaTime, 0, 0f));
         if (transform.position.y < -5)
         {
             Destroy(this.gameObject);
