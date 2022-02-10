d64f429bcb1325ac4fc903165e74b514aea6dbe3
simkaiwen <kaiphonegt@gmail.com>
2022-02-10 17:58:05 +0800

    Created SceneHandler script

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public static SceneHandler inst;
+
+    private void Start()
+    {
+        inst = this;    
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
