65519ad32d5235aa9a9878ea923009f47b1e6269
simkaiwen <kaiphonegt@gmail.com>
2022-02-10 20:23:14 +0800

    Added Score functions to Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    private int score;
+
     public float speed;
     public float yLimit;
+    public Text scoreText;
 
     void Start()
     {
-
+        
     }
 
     void Update()
@@ -19,6 +23,16 @@ public class Player : MonoBehaviour
 
         transform.position = new Vector3(transform.position.x,
             Mathf.Clamp(transform.position.y, -yLimit, yLimit), transform.position.z);
+    }
 
+    public void UpdateScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public float GetScore()
+    {
+        return System.Convert.ToSingle(score);
     }
 }
